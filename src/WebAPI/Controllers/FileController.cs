d5faed4e5afe2c56db69ad53e55ac9d80b906b72
Степан Догель <s.dogel@smb.by>
2024-04-09 14:55:48 +0300

    Init

---
+++ src/WebAPI/Controllers/FileController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace WebAPI.Controllers
+{
+    public class FileController : ControllerBase
+    {
+        [HttpGet()]
+        public async Task<ActionResult> GetUserCatalogs(CancellationToken ct)
+        {
+            return Ok();
+        }
+
+    }
+}
---
+++ src/WpfClientApp/NewFolder/CreateItemForm.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfClientApp.NewFolder
+{
+    /// <summary>
+    /// Interaction logic for CreateItemForm.xaml
+    /// </summary>
+    public partial class CreateItemForm : Window
+    {
+        public CreateItemForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.ShowDialog();
+
+            var FilePath = openFileDialog.FileName;
+
+            var bitMapImg = new BitmapImage((new Uri(FilePath)));
+
+            imgPreview.Source = bitMapImg;
+
+            byte[] data;
+            JpegBitmapEncoder encoder = new JpegBitmapEncoder();
+            encoder.Frames.Add(BitmapFrame.Create(bitMapImg));
+            using (MemoryStream ms = new MemoryStream())
+            {
+                encoder.Save(ms);
+                data = ms.ToArray();
+            }
+        }
+    }
+}
